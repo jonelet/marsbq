fc554e2c617fe6cb94773633dfbd36bf3c34a895
jonelet <jonelet@gmail.com>
2017-03-29 20:32:15 +0200

    implement countdown clock

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,32 @@
+var countdown = require('countdown');
+
 var model = {
     init: function () {
         console.log('Model initialized');
-    }
+    },
+
+    currentDate: new Date(),
+    eventDate: new Date(2056, 10, 05)
 };
 
 var helper = {
 
+  dateDiff: function() {
+    return countdown(model.currentDate, model.eventDate);
+  }
 };
 
 var view = {
     init: function () {
-        console.log('View initialized');
+        view.showCountDown();
+    },
+
+    showCountDown: function() {
+      var diff = helper.dateDiff();
+
+      document.querySelector('#years').innerHTML = diff.years;
+      document.querySelector('#days').innerHTML = diff.months;
+      document.querySelector('#hours').innerHTML = diff.days;
     }
 };
 
